2e407e79c34ffca72aa83732f4560bcec9375cba
Rishab Garg <rishgarg@gmail.com>
2022-05-05 09:02:23 +0200

    Fixed colour in main class

--- src/roomplanner/Main.java
+++ src/roomplanner/Main.java
@@ -1,5 +1,7 @@
 package roomplanner;
 
+import java.awt.Color;
+
 import objects.Chair;
 import objects.Table;
 import objects.Wardrobe;
@@ -9,7 +11,7 @@ public class Main {
 	public static void main(String[] args) {
 		Canvas.giveCanvas();
 		Table table = new Table();
-		table.changeColour("red");
+		table.changeColour(Color.RED);
 		Wardrobe wardrobe = new Wardrobe(300, 100, 3);
 		wardrobe.MoveX(200);
 		Chair chair = new Chair();
